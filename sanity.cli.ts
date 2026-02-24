fe71a4ef9ea1997768d2a01488ea996275aaaec4
Masmoud Yacoubou <159589209+masmoud-yacoubou@users.noreply.github.com>
2026-02-24 19:39:50 +0100

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -7,4 +7,11 @@ import { defineCliConfig } from 'sanity/cli'
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig(
+  { 
+    api: { projectId, dataset },
+    deployment: {
+    appId: 'wgcglyoknwb57i4uv0yvalke',
+    autoUpdates: true
+  }
+  })
